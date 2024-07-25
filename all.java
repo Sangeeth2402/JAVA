99f2707ac315846d976204bfebd56cf29e9c7f8c
Sangeeth2402 <146965484+Sangeeth2402@users.noreply.github.com>
2024-07-25 09:31:07 +0530

    Add files via upload

---
+++ all.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class all {
+    public static void main(String[] args){
+        int a,b,c,d,e,f;
+        a=10;
+        b=20;
+        c=10;
+        d=a*b*c;
+        e=a+b;
+        f=d/e;
+        System.out.print(e);
+    }
+}
